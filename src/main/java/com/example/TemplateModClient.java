967a702aa9f3053ccc855ea8ad31e69b954f7839
eddie <eddie@eddiedeMacBook-Air.local>
2025-09-28 00:33:58 -0700

    Initial commit

---
+++ src/main/java/com/example/TemplateModClient.java
@@ -0,0 +1,8 @@
+package com.example;
+import net.fabricmc.api.ClientModInitializer;
+public class TemplateModClient implements  ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-side initialization code here
+    }
+}
